5e2a9e3b31b6e9d07c80479759c1ad7c1b616693
MajdDaou <127030801+Majd1155-web@users.noreply.github.com>
2024-09-06 20:18:52 +0300

    remove unused imports

--- src/main/java/com/example/internshipBackend/Mapper/LeaveTypeMapper.java
+++ src/main/java/com/example/internshipBackend/Mapper/LeaveTypeMapper.java
@@ -1,8 +1,6 @@
 package com.example.internshipBackend.Mapper;
 
-import com.example.internshipBackend.DTO.LeaveDTO;
 import com.example.internshipBackend.DTO.LeaveTypeDTO;
-import com.example.internshipBackend.entity.LeaveEntity;
 import com.example.internshipBackend.entity.LeavetypeEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
--- src/main/java/com/example/internshipBackend/entity/ExpenseclaimentryEntity.java
+++ src/main/java/com/example/internshipBackend/entity/ExpenseclaimentryEntity.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.sql.Date;
-import java.util.Objects;
 
 @Setter
 @Getter
