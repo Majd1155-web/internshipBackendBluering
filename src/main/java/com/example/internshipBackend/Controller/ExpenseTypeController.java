a71e3c4d90d082e71f431f53fe287d789f46fbe8
MajdDaou <127030801+Majd1155-web@users.noreply.github.com>
2024-09-09 16:44:22 +0300

    expense type service and controller working

--- src/main/java/com/example/internshipBackend/Controller/ExpenseTypeController.java
+++ src/main/java/com/example/internshipBackend/Controller/ExpenseTypeController.java
@@ -1,4 +1,54 @@
 package com.example.internshipBackend.Controller;
 
+import com.example.internshipBackend.Service.ExpenseTypeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("expensetype")
+@CrossOrigin("*")
 public class ExpenseTypeController {
+
+    private final ExpenseTypeService expenseTypeService;
+
+    public ExpenseTypeController(ExpenseTypeService expenseTypeService) {
+        this.expenseTypeService = expenseTypeService;
+    }
+
+    @PostMapping("createExpenseType")
+    @ResponseStatus(HttpStatus.OK)
+    public String CreateExpenseType(@RequestBody Map<String, Object> expenseTypeDTO) {
+        expenseTypeService.CreateExpenseType(expenseTypeDTO);
+        return "expense type created successfully";
+    }
+
+    @GetMapping("getExpenseTypes")
+    @ResponseStatus(HttpStatus.OK)
+    public List<?> GetExpenseType() {
+        return expenseTypeService.GetExpenseType();
+    }
+
+    @GetMapping("getExpenseTypeById/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<?> GetExpenseTypeById(@PathVariable Integer id) {
+        return expenseTypeService.GetExpenseTypeById(id);
+    }
+
+    @PatchMapping("updateExpenseType/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public String UpdateExpenseType(@PathVariable Integer id, @RequestBody Map<String, Object> expenseTypeDTO) {
+        expenseTypeService.UpdateExpenseType(id, expenseTypeDTO);
+        return "expense type updated successfully";
+    }
+
+    @DeleteMapping("deleteExpenseType/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public String DeleteExpenseType(@PathVariable Integer id) {
+        expenseTypeService.DeleteExpenseType(id);
+        return "expense type deleted successfully";
+    }
+
 }
--- src/main/java/com/example/internshipBackend/Service/ExpenseTypeService.java
+++ src/main/java/com/example/internshipBackend/Service/ExpenseTypeService.java
@@ -1,4 +1,66 @@
 package com.example.internshipBackend.Service;
 
+import com.example.internshipBackend.DTO.ExpenseTypeDTO;
+import com.example.internshipBackend.Mapper.ExpenseTypeMapper;
+import com.example.internshipBackend.Repository.ExpenseTypeRepository;
+import com.example.internshipBackend.entity.ExpensetypeEntity;
+import org.hibernate.query.sqm.mutation.internal.temptable.LocalTemporaryTableInsertStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
 public class ExpenseTypeService {
+
+    @Autowired
+    private GeneralService generalService;
+
+    @Autowired
+    private ExpenseTypeMapper expenseTypeMapper;
+
+    private final ExpenseTypeRepository expenseTypeRepository;
+
+    public ExpenseTypeService(ExpenseTypeRepository expenseTypeRepository) {
+        this.expenseTypeRepository = expenseTypeRepository;
+    }
+
+    public void CreateExpenseType(Map<String, Object> expenseTypeDTO) {
+        ExpensetypeEntity expenseType = new ExpensetypeEntity();
+        generalService.updateEntity(expenseTypeDTO, expenseType, ExpensetypeEntity.class);
+        expenseTypeRepository.saveAndFlush(expenseType);
+    }
+
+    public List<ExpenseTypeDTO> GetExpenseType() {
+        return expenseTypeRepository.findAll()
+                .stream()
+                .map(expenseType -> {
+                    ExpenseTypeDTO expenseTypeDTO = expenseTypeMapper.ExpenseTypeEntityToExpenseTypeDTO(expenseType);
+                    return expenseTypeDTO;
+                })
+                .collect(Collectors.toList());
+    }
+
+    public List<ExpenseTypeDTO> GetExpenseTypeById(Integer id) {
+        return expenseTypeRepository.findById(id)
+                .stream()
+                .map(expenseType -> {
+                    ExpenseTypeDTO expenseTypeDTO = expenseTypeMapper.ExpenseTypeEntityToExpenseTypeDTO(expenseType);
+                    return expenseTypeDTO;
+                })
+                .collect(Collectors.toList());
+    }
+
+    public void UpdateExpenseType(Integer id, Map<String, Object> expenseTypeDTO) {
+        ExpensetypeEntity expenseType = expenseTypeRepository.findById(id).get();
+        generalService.updateEntity(expenseTypeDTO, expenseType, ExpensetypeEntity.class);
+        expenseTypeRepository.saveAndFlush(expenseType);
+    }
+
+    public void DeleteExpenseType(Integer id){
+        expenseTypeRepository.deleteById(id);
+    }
+
 }
