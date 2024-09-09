25d855356027b4db263637fe60c8e37f84e1fe69
MajdDaou <127030801+Majd1155-web@users.noreply.github.com>
2024-09-09 21:08:02 +0300

    Update LeaveMapper.java

--- src/main/java/com/example/internshipBackend/Mapper/LeaveMapper.java
+++ src/main/java/com/example/internshipBackend/Mapper/LeaveMapper.java
@@ -3,6 +3,7 @@ package com.example.internshipBackend.Mapper;
 import com.example.internshipBackend.DTO.LeaveDTO;
 import com.example.internshipBackend.entity.LeaveEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,12 @@ import org.springframework.stereotype.Component;
 @Component
 public interface LeaveMapper {
     LeaveMapper INSTANCE = Mappers.getMapper(LeaveMapper.class);
+
+    @Mapping(target = "from", expression = "java(new java.util.Date(leaveEntity.getFrom().getTime()))")
+    @Mapping(target = "to", expression = "java(new java.util.Date(leaveEntity.getTo().getTime()))")
     LeaveDTO LeaveEntityToLeaveDTO(LeaveEntity leaveEntity);
+
+    @Mapping(target = "from", expression = "java(new java.sql.Date(leaveDTO.getFrom().getTime()))")
+    @Mapping(target = "to", expression = "java(new java.sql.Date(leaveDTO.getTo().getTime()))")
     LeaveEntity LeaveDTOToLeaveEntity(LeaveDTO leaveDTO);
 }
